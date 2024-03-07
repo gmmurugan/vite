0e1e3442df867c794bc877bd666bd3190e252832
gmmurugan <gmmurugan@gmail.com>
2024-03-06 23:22:03 -0600

    jest test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import './App.css'
 import StopWatch from './components/Stopwatch'
 
 function App() {
 
   return (
-    <ChakraProvider>
       <StopWatch/>
-      </ChakraProvider>
   )
 }
 
